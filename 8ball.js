a7b0e90e53a8586e01d9498f3299c5148e9a376e
AlphaRacer <tarun.a1441@gmail.com>
2020-07-04 12:14:30 +0530

    Massive update

---
+++ 8ball.js
@@ -0,0 +1,13 @@
+function eightball() {
+    const replies = ['Yes', 'No', 'I don\'t know', 'Probably', 'Probably not', 'I don\'t think so', 'Nope', 'Definitely', 'Of course'];
+
+    const result = Math.floor((Math.random() * replies.length));
+
+    var question = prompt("Please enter your question:", "");
+    if (question == null || question == "") {
+        alert("Please ask a full question!");
+    } else {
+        // txt = "Question: " + question + "\nAnswer: "  + replies[result]
+        alert("Question: " + question + "\nAnswer: " + replies[result]);
+    }
+}
--- conflip.js
+++ conflip.js
@@ -1,5 +1,4 @@
 function printMessage() {
-    var x = document.getElementById("button1")
     const replies = ['Heads', 'Tails'];
 
     const result = Math.floor((Math.random() * replies.length));
